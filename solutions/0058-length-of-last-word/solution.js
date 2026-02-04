618dc06f98ca87815d1095d30dc8157b099e4587
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-02-04 05:31:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 54 MB (28.54%)

---
+++ solutions/0058-length-of-last-word/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let count = 0;
+    let i = s.length - 1;
+
+    while(i >= 0 && s[i] ===  " "){
+        i--;
+    }
+    while(i >= 0 && s[i] !== " "){
+        count++;
+        i--;
+    }
+    return count;
+};
