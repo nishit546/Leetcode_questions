3925a00f257ddbcb2e4ee2b63229d0d1e336fa5d
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-02-13 12:17:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 57.5 MB (5.77%)

---
+++ solutions/3515-find-if-digit-game-can-be-won/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canAliceWin = function(nums) {
+    let singleSum = 0;
+    let doubleSum = 0;
+    for(let i = 0;i<nums.length;i++){
+        if(nums[i] < 10){
+            singleSum += nums[i];
+        }
+        else{
+            doubleSum += nums[i];
+        }
+    }
+   return singleSum !== doubleSum;
+};
