03db15255d62ed3fca2424754d1fe415f18d85ff
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-02-14 04:35:12 +0000

    Sync LeetCode submission Runtime - 1 ms (55.64%), Memory - 43.7 MB (58.13%)

---
+++ solutions/3321-type-of-triangle/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String triangleType(int[] nums) {
+         if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]){
+
+         if(nums[0] == nums[1] && nums[1] == nums[2]){
+    return "equilateral";
+   }
+        if(nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]){
+            return "isosceles";
+        }
+        else if(nums[0] != nums[1] && nums[1] != nums[2] && nums[2] != nums[0]){
+            return "scalene";
+        }
+    }
+    
+       return "none"; 
+    }
+    
+}
