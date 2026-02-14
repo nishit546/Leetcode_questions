3dfd1803498a6b129cc779612a1293cda700bff4
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-02-14 04:30:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 55.7 MB (21.67%)

---
+++ solutions/3321-type-of-triangle/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+  
+    if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]){
+
+         if(nums[0] === nums[1] && nums[1] === nums[2]){
+    return "equilateral";
+   }
+        if(nums[0] === nums[1] || nums[1] === nums[2] || nums[0] === nums[2]){
+            return "isosceles";
+        }
+        else if(nums[0] != nums[1] && nums[1] != nums[2] && nums[2] != nums[0]){
+            return "scalene";
+        }
+    }
+    else{
+        return "none";
+    }
+};
