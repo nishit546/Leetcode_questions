b967f4b8f1b755db9cd69e0b6116e48dedd73f9c
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-03-07 04:33:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 55.3 MB (77.91%)

---
+++ solutions/0171-excel-sheet-column-number/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let res = 0;
+    for(let i = 0;i<columnTitle.length;i++){
+        res = res * 26 + (columnTitle.charCodeAt(i) - 64);
+    }
+    return res;
+};
