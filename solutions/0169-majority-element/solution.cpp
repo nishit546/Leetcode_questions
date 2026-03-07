22f02084a99594c50a0c13dad8b4d668913ded12
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-03-07 05:02:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.2 MB (69.75%)

---
+++ solutions/0169-majority-element/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int dig = 0;
+        int count = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(count == 0){
+                dig = nums[i];
+            }
+             if(dig == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return dig;
+    }
+};
