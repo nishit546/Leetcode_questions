9fcdc35704f248548cde6e737a15e58d7bb18429
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-04-06 04:58:08 +0000

    Sync LeetCode submission Runtime - 71 ms (14.03%), Memory - 88 MB (9.54%)

---
+++ solutions/0287-find-the-duplicate-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+   unordered_map<int,int> freq;
+   for(int i = 0;i<nums.size();i++){
+    if(!freq[nums[i]]){
+        freq[nums[i]] = 1;
+    }
+    else{
+        return nums[i];
+    }
+   }
+return -1;
+
+}
+};
