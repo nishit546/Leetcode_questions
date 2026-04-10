0d7a31ce4131ad7a1f0e2439df945d40a9fe0adc
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-04-10 09:19:26 +0000

    Sync LeetCode submission Runtime - 11 ms (91.49%), Memory - 24.9 MB (88.22%)

---
+++ solutions/0049-group-anagrams/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        string temp;
+        unordered_map <string,vector <string>>category;
+        for(string word : strs){
+            temp = word;
+            sort(temp.begin(),temp.end());
+            category[temp].push_back(word);
+        }
+        vector<vector<string>>result;
+        for(auto& it : category){
+            result.push_back(it.second);
+        }
+        return result;
+    }
+};
