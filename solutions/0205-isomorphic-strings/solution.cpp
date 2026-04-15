7fb65e644e32a905fa02bf0eafdf310ebcd2ae00
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-04-15 05:03:48 +0000

    Sync LeetCode submission Runtime - 3 ms (41.00%), Memory - 9.5 MB (7.62%)

---
+++ solutions/0205-isomorphic-strings/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char,int>m1;
+        unordered_map<char,int>m2;
+        for(int i = 0;i<s.length();i++){
+            if(m1[s[i]] != m2[t[i]]){
+                return false;
+            }
+       m1[s[i]] = i+1;
+       m2[t[i]] = i+1;
+        }
+        return true;
+        }
+    
+};
