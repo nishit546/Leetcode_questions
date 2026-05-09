b7bffc5d083a4e279519e37d2b44329535da2120
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-05-09 04:30:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (93.10%)

---
+++ solutions/3636-check-balanced-string/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+      int oddSum = 0;
+      int evenSum = 0;
+      for(int i = 0;i < num.size();i++){
+        int digit = num[i] - '0';
+        if(i % 2 == 0){
+            evenSum += digit;
+        }
+        else{
+            oddSum += digit;
+        }
+      }
+      if(oddSum == evenSum){
+        return true;
+      }
+      return false;
+    }
+};
