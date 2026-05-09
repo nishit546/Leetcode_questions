e7eb8830fb85fdb91284963fafa414a7a500db34
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-05-09 08:04:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (79.51%)

--- solutions/0014-longest-common-prefix/solution.cpp
+++ solutions/0014-longest-common-prefix/solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string pre = strs[0];
-        for(int i = 1;i<strs.size();i++){
-            while(strs[i].find(pre) != 0){
-                pre.pop_back();
-            }
+      if(strs.empty()){
+        return "";
+      }
+      string pre = strs[0];
+      for(int i = 1;i<strs.size();i++){
+        while(strs[i].find(pre) != 0){
+            pre.pop_back();
         }
-        return pre;
+      }
+      return pre;
     }
 };
