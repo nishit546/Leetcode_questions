fd8abb2fa596bb851f36bbaf68a4f61bf7b7c114
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-05-10 06:33:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (18.17%)

--- solutions/0412-fizz-buzz/solution.cpp
+++ solutions/0412-fizz-buzz/solution.cpp
@@ -3,17 +3,18 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> answer;
 
-        for(int i = 1; i <= n; i++){
-            if(i % 3 == 0 && i % 5 == 0){
+        for(int i = 1; i <= n; i++) {
+
+            if(i % 3 == 0 && i % 5 == 0) {
                 answer.push_back("FizzBuzz");
             }
-            else if(i % 3 == 0){
+            else if(i % 3 == 0) {
                 answer.push_back("Fizz");
             }
-            else if(i % 5 == 0){
+            else if(i % 5 == 0) {
                 answer.push_back("Buzz");
             }
-            else{
+            else {
                 answer.push_back(to_string(i));
             }
         }
