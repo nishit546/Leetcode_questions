d06d26409423fddb20c7da6a0ab4b371792b6946
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-05-12 12:46:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (12.37%)

--- solutions/0231-power-of-two/solution.cpp
+++ solutions/0231-power-of-two/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<1){
+        if(n < 1){
             return false;
         }
-        while(n %2 == 0){
+        while(n % 2 == 0){
             n = n / 2;
         }
         return n == 1;
