abc39239a824a2b5f8ef1dfb5a02831622645748
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-05-27 11:41:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.9 MB (7.84%)

--- solutions/1791-richest-customer-wealth/solution.cpp
+++ solutions/1791-richest-customer-wealth/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-       int maximum = 0;
-       for(auto customer : accounts){
+      int maximum = 0;
+      for(auto customer : accounts){
         int sum = 0;
         for(int money : customer){
             sum += money;
-            
         }
         maximum = max(maximum,sum);
-       } 
-       return maximum;
+      }
+      return maximum;
     }
 };
