6575e3a7915074b802c10129c9cece3156d3fbab
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-06-09 09:49:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (54.89%)

---
+++ solutions/1078-remove-outermost-parentheses/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int count = 0;
+
+        for(char ch : s) {
+            if(ch == '(') {
+                if(count > 0)
+                    res += ch;
+                count++;
+            }
+            else {
+                count--;
+                if(count > 0)
+                    res += ch;
+            }
+        }
+
+        return res;
+    }
+};
