798d55c209bcc65865981f861e69887b7593adf0
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-07-17 10:00:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.1 MB (16.76%)

--- solutions/1319-unique-number-of-occurrences/solution.cpp
+++ solutions/1319-unique-number-of-occurrences/solution.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-      unordered_map<int, int>count;
-      for(int num : arr){
-        count[num]++;
-      }
-    vector<int> freq;
-    for(auto const&[key,val] : count){
+        unordered_map<int,int> count;
+        for(int num:arr){
+            count[num]++;
+        }
+    vector<int>freq;
+    for(auto const&[key,val]:count){
         freq.push_back(val);
     }
-sort(freq.begin(),freq.end());
-    for(int i = 1;i<freq.size();++i){
-        if(freq[i] == freq[i-1]){
-            return false;
-        }
+    sort(freq.begin(),freq.end());
+    for(int i = 1;i<freq.size();i++){
+       if(freq[i] == freq[i-1]){
+        return false;
+       } 
     }
     return true;
     }
