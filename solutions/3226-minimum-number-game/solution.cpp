fe1bb7f69902b90a3651f5fb1c4a892836819b68
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-07-28 09:15:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.2 MB (77.88%)

--- solutions/3226-minimum-number-game/solution.cpp
+++ solutions/3226-minimum-number-game/solution.cpp
@@ -1,18 +1,13 @@
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i + 1 < nums.size(); i += 2) {
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<nums.size()-1;i+=2){
             int temp = nums[i];
-            nums[i] = nums[i + 1];
-            nums[i + 1] = temp;
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
         }
-
         return nums;
     }
+    
 };
-
