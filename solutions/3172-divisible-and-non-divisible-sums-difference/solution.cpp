48c9a8824aac3cff5c7c886c5b3a875ac776b785
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-07-30 10:18:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (66.24%)

--- solutions/3172-divisible-and-non-divisible-sums-difference/solution.cpp
+++ solutions/3172-divisible-and-non-divisible-sums-difference/solution.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int differenceOfSums(int n, int m) {
         int num1 = 0;
-        int num2  = 0;
-        int i = 1;
-        for(;i<=n;i++){
+        int num2 = 0;
+        for(int i =1 ;i<=n;i++){
             if(i % m != 0){
-                num1+=i;
+                num1 += i;
             }
             else{
-                num2+=i;
+                num2 += i;
             }
-        }    
-        return num1-num2;  
+        }
+        return num1-num2;
     }
 };
