42ee59dc18294f03015d7692f82780ccc13e7daa
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-08-07 10:24:04 +0000

    Sync LeetCode submission Runtime - 2 ms (11.15%), Memory - 15.6 MB (94.35%)

---
+++ solutions/0082-remove-duplicates-from-sorted-list-ii/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+
+            if (curr->next != nullptr && curr->val == curr->next->val) {
+
+                while (curr->next != nullptr &&
+                       curr->val == curr->next->val) {
+                    curr = curr->next;
+                }
+                prev->next = curr->next;
+            }
+            else {
+                prev = curr;
+            }
+
+            curr = curr->next;
+        }
+
+        return dummy->next;
+    }
+};
