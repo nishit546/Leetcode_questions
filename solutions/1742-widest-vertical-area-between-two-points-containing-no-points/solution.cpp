0df4a2f515e72907743dcad3ba5c9157510b9c1d
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-08-12 09:43:02 +0000

    Sync LeetCode submission Runtime - 31 ms (52.26%), Memory - 70.5 MB (47.86%)

---
+++ solutions/1742-widest-vertical-area-between-two-points-containing-no-points/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(points.begin(),points.end());
+        int maxWidth = 0;
+        for(int i = 1;i<points.size();i++){
+           int width = points[i][0] - points[i-1][0];
+            maxWidth = max(width,maxWidth);
+        }
+       
+        return maxWidth;
+    }
+};
