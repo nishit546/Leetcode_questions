b912eb95898cdd83e632f365d655e32642cedef2
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-08-18 10:51:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.7 MB (48.93%)

---
+++ solutions/1950-sign-of-the-product-of-an-array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int negativeCount = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                return 0;
+            }
+
+            if (nums[i] < 0) {
+                negativeCount++;
+            }
+        }
+
+        if (negativeCount % 2 == 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+};
