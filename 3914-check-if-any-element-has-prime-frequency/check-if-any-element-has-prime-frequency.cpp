cf4260ad6846f9972458911e673bf17411679d83
Nishit Doshi <nishit.g.doshi.cg@gmail.com>
2026-09-02 17:28:14 +0530

    Time: 1 ms (44.04%) | Memory: 24.6 MB (8.19%) - LeetSync

---
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+bool isPrime(int n){
+    if(n < 2){
+        return false;
+    }
+    for(int i = 2;i*i<=n;i++){
+        if(n % i == 0){
+            return false;
+        }
+
+    }
+    return true;
+}
+    bool checkPrimeFrequency(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int num : nums){
+            freq[num]++;
+        }
+        for(auto it: freq){
+            if(isPrime(it.second)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+};
