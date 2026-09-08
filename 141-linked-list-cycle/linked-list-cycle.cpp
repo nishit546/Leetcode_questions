1a9d418acc253e08e0b2ff527f2951ec6b869bb9
Nishit Doshi <nishit.g.doshi.cg@gmail.com>
2026-09-08 11:29:31 +0530

    Time: 12 ms (41.12%) | Memory: 11.8 MB (81.05%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,15 +9,28 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-      set<ListNode*> address;
-      ListNode* i = head;
-      while(i != NULL){
-        if(address.count(i)){
-            return true;
-        }
-        address.insert(i);
-        i = i -> next;
-      }
+//       set<ListNode*> address;
+//       ListNode* i = head;
+//       while(i != NULL){
+//         if(address.count(i)){
+//             return true;
+//         }
+//         address.insert(i);
+//         i = i -> next;
+//       }
+// return false;
+
+// Solution 2 
+ListNode* i = head;
+ListNode* j = head;
+while(j != NULL && j->next != NULL){
+    i=i->next;
+    j=j->next->next;
+    if(i == j){
+        return true;
+    }
+}
 return false;
+    
     }
 };
